ba2c480cef93268082ef97cdb1dcc9300f328e32
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2022-08-27 21:36:51 +0900

    Update sample_test2.py

--- sample_test2.py
+++ sample_test2.py
@@ -5,5 +5,5 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5"
 
 
 #train(beta_min=1, alpha=2,beta_max=20,n_epochs=1000)
-sample(path ='/home/eunbiyoon/Levy_motion-/2_0.01_2.0.pth',beta_min=1,
+sample(path ='2_0.01_2.0.pth',beta_min=1,
        beta_max=20, sampler='pc_sampler2', num_steps=1000,LM_steps=2,Predictor=True, Corrector=False)
