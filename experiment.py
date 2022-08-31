a6245b91923ec5e6dc5a2f534922afa2caecd143
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2022-09-01 08:20:33 +0900

    Delete experiment.py

--- experiment.py
+++
@@ -1,9 +0,0 @@
-from training import *
-from sampling import *
-import numpy as np
-import os
-
-for beta_min in np.linspace(0.1,1, 10)[::-1]:
-    for beta_max in np.linspace(10,20,10)[::-1]:
-        train(alpha=2,beta_min=beta_min, beta_max=beta_max, n_epochs=100)
-        sample(alpha=2, beta_min=beta_min, beta_max = beta_max)
