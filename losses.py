6c48fec5201e9174c02e93021a6a0678ab651bad
eunbi1 <82250837+eunbi1@users.noreply.github.com>
2022-09-10 16:33:32 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -25,21 +25,24 @@ def loss_fn(model, sde,
             x0: torch.Tensor,
             t: torch.LongTensor,
             e_L: torch,
-            num_steps=1000, type="cft", mode='only'):
+            num_steps=1000, type="backpropagation", mode='only'):
     sigma = sde.marginal_std(t)
     x_coeff = sde.diffusion_coeff(t)
 
     if sde.alpha == 2:
-      score = -1 / 2 * (e_L)*torch.pow(sigma+1e-4,-1)[:,None,None,None]*sde.beta(t)[:,None,None,None]
+      score = -1 / 2 * (e_L)
+
 
     else:
-        score = levy.score(e_L, sde.alpha, type=type).to(device)* torch.pow(sigma+1e-4,-1)[:,None,None,None]*sde.beta(t)[:,None,None,None]
+        score = levy.score(e_L, sde.alpha, type=type).to(device)
+
+
 
     x_t = x_coeff[:, None, None, None] * x0 + e_L * sigma[:, None, None, None]
-    output = model(x_t, t) * sde.beta(t)[:, None, None, None]
+    output = model(x_t, t)
     weight = (output - score)
-    loss = (weight).square().sum(dim=(1, 2, 3)).mean(dim=0)
-
+    #loss = torch.abs(weight).sum(dim=(1,2,3)).mean(dim=0)
+    loss=(weight).square().sum(dim=(1, 2, 3)).mean(dim=0)
     #print('x_t', torch.min(x_t), torch.max(x_t))
     #print('e_L', torch.min(e_L),torch.max(e_L))
     #print('score', torch.min(score), torch.max(score))
@@ -47,4 +50,39 @@ def loss_fn(model, sde,
     #print('output*beta',torch.min(output), torch.max(output))
     #print('weight', torch.min(weight), torch.max(weight))
 
-    return  loss
+    return loss
+
+
+def loss_fn(model, sde,
+            x0: torch.Tensor,
+            t: torch.LongTensor,
+            e_L: torch,
+            num_steps=1000, type="cft", mode='only'):
+    sigma = sde.marginal_std(t)
+    x_coeff = sde.diffusion_coeff(t)
+    beta = sde.beta(t)[:,None,None,None]
+
+    if sde.alpha == 2:
+      #score = -1 / 2 * (e_L)*torch.pow(sigma+1e-5,-1)[:,None,None,None]*beta
+      score = -1 / 2 * (e_L)
+
+
+    else:
+        #score = levy.score(e_L, sde.alpha, type=type).to(device)* torch.pow(sigma+1e-5,-1)[:,None,None,None]*beta
+        score = levy.score(e_L, sde.alpha, type=type).to(device)
+
+
+
+    x_t = x_coeff[:, None, None, None] * x0 + e_L * sigma[:, None, None, None]
+    output = model(x_t, t)
+    weight = (output - score)
+    #loss = torch.abs(weight).sum(dim=(1,2,3)).mean(dim=0)
+    loss=(weight).square().sum(dim=(1, 2, 3)).mean(dim=0)
+    #print('x_t', torch.min(x_t), torch.max(x_t))
+    #print('e_L', torch.min(e_L),torch.max(e_L))
+    #print('score', torch.min(score), torch.max(score))
+    #print('output', torch.min(model(x_t, t)), torch.max(model(x_t, t)))
+    #print('output*beta',torch.min(output), torch.max(output))
+    #print('weight', torch.min(weight), torch.max(weight))
+
+    return loss
